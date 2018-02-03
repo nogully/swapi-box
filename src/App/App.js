346e42b5963de8ec40d28279138b8f4f4ceac803
Nora Gully <nora.gully@gmail.com>
2018-02-02 20:27:52 -0700

    Refactor button without Button class. Pass object back from favorites button
    to populate state.favorites array directly. Refactor Header to take button elements. Refactor Card to display data conditionally.

--- src/App/App.js
+++ src/App/App.js
@@ -17,7 +17,7 @@ class App extends Component {
   fetchData = async (event) => {
     console.log(event.target.textContent)
     switch (event.target.textContent) {
-      case 'People': 
+      case 'people': 
         if (!this.state.people) {
           const people = await getPeople()
           this.setState({ 
@@ -27,7 +27,7 @@ class App extends Component {
             category: 'people' })
         } else { this.setState({category: 'people'}) }
         break;
-      case 'Planets':
+      case 'planets':
         if (!this.state.planets) {
           const planets = await getPlanets()
           this.setState({ 
@@ -37,7 +37,7 @@ class App extends Component {
             category: 'planets' })
         } else { this.setState({category: 'planets'}) }
         break;
-      case 'Vehicles':
+      case 'vehicles':
         if (!this.state.vehicles) {
           const vehicles = await getVehicles()
           this.setState({ 
@@ -56,24 +56,19 @@ class App extends Component {
     this.setState({ randomFilm })
   }
 
-  favoriteCard = (event) => {
-    const favCardKey = event.target.name;
-    if (!this.state.favorites.includes(favCardKey)) {
+  favoriteCard = (card) => {
+    if (!this.state.favorites.includes(card)) {
       this.setState({ 
-        favorites: [...this.state.favorites, favCardKey] })
+        favorites: [...this.state.favorites, card] })
     } else {
       this.setState({ 
-        favorites: [...this.state.favorites.filter(key => !favCardKey)]})
+        favorites: [...this.state.favorites.filter(favorite => favorite !== card)]})
     }
   }
 
-  displayFavorites = () => {
-    // if (this.state.favorites.length) {
-    //   return 
-    // } else {
-
-    // }
-    console.log(this.state.favorites)
+  displayFavorites = (event) => { 
+    const category = 'favorites'
+    this.setState({ category })
   }
 
 
--- src/Card/Card.js
+++ src/Card/Card.js
@@ -2,27 +2,31 @@ import React from 'react';
 import Button from '../Button/Button'
 import './Card.css'
 
-const Card = (props) => {
+const Card = ({ data, favoriteCard, isActive, key }) => {
+  const keys = Object.keys(data).filter( key => key !== 'name')
+  const elements = keys.map( (key, index) => {
+    return <p key={index}>{`${key} : ${data[key]}`}</p>
+  })
 
   return (
-    <article className={"Card " + props.active} >
-      <h1>{props.name}</h1>
-      <h3>{props.species}</h3>
-      <h3>{props.homeworld}</h3>
-      <p>{props.population}</p>
-      <p>{props.terrain}</p>
-      <p>{props.climate}</p>
-      <p>{props.residents}</p>
-      <p>{props.model}</p>
-      <p>{props.vehicle_class}</p>
-      <p>{props.passengers}</p>
-      <Button buttonType='light'
-              buttonText='Favorite'
-              function={props.favoriteCard}
-              name={props.name} />
+    <article className={ "Card " + isActive } >
+      <h1>{ data.name }</h1>
+
+      { elements }
+      
+      <button onClick={() => favoriteCard(data)} 
+              className='light'> 
+        favorite
+      </button>
     </article>
   )
 
 }
 
 export default Card;
+
+   // <Button buttonType='light'
+   //            buttonText='Favorite'
+   //            buttonFunction={ favoriteCard }
+   //            data={ data }
+   //            name={ data.name } />
